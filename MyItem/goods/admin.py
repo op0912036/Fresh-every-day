61cd384d6d5e5b0091dd74f76fc77a1e2f97ecd0
程序猿 <op0912036@163.com>
2018-10-23 16:41:15 +0800

    主页静态化

--- MyItem/goods/admin.py
+++ MyItem/goods/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from goods.models import *
 
+
 # Register your models here.
-admin.site.register(GoodsType)
-admin.site.register(GoodsSPU)
-admin.site.register(GoodsSKU)
+class BaseAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        '''新增或更新表中的数据时调用'''
+        super().save_model(request, obj, form, change)
+        from celery_tasks.tasks import task_generate_static_index
+        task_generate_static_index.delay()
+
+    def delete_mode(self, request, obj):
+        '''删除表中的数据时调用'''
+        super().delete_model(request, obj)
+        from celery_tasks.tasks import task_generate_static_index
+        task_generate_static_index.delay()
+
+
+admin.site.register(GoodsType, BaseAdmin)
+admin.site.register(GoodsSPU, BaseAdmin)
+admin.site.register(GoodsSKU, BaseAdmin)
 admin.site.register(GoodsImage)
-admin.site.register(IndexGoodsBanner)
-admin.site.register(IndexTypeGoodsBanner)
-admin.site.register(IndexPromotionBanner)
+admin.site.register(IndexGoodsBanner, BaseAdmin)
+admin.site.register(IndexTypeGoodsBanner, BaseAdmin)
+admin.site.register(IndexPromotionBanner, BaseAdmin)
